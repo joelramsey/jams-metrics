328654257dfb74558d1df3b6942642974aeddbc1
joelramsey <joel.d.ramsey@gmail.com>
2015-10-19 18:04:19 -0600

    WIP - added nvd3 but getting error in console

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,4 @@
-var jamsMetrics = angular.module('jamsMetrics', ['ui.router']);
+var jamsMetrics = angular.module('jamsMetrics', ['ui.router', 'nvd3']);
 
 jamsMetrics.config(function ($stateProvider, $locationProvider) {
     $locationProvider.html5Mode({
@@ -264,6 +264,75 @@ jamsMetrics.factory('Player', function () {
 jamsMetrics.controller('Metrics', ['$scope', 'Player', 'Metric', function ($scope, Player, Metric) {
 
     $scope.songs = Metric.listSongsPlayed();
+
+    $scope.options = {
+        chart: {
+            type: 'discreteBarChart',
+            height: 450,
+            margin: {
+                top: 20,
+                right: 20,
+                bottom: 60,
+                left: 55
+            },
+            x: function (d) {
+                return d.label;
+            },
+            y: function (d) {
+                return d.value;
+            },
+            showValues: true,
+            valueFormat: function (d) {
+                return d3.format(',.4f')(d);
+            },
+            transitionDuration: 500,
+            xAxis: {
+                axisLabel: 'X Axis'
+            },
+            yAxis: {
+                axisLabel: 'Y Axis',
+                axisLabelDistance: 30
+            }
+        }
+    };
+
+    $scope.data = [{
+        key: "Cumulative Return",
+        values: [
+            {
+                "label": "A",
+                "value": -29.765957771107
+            },
+            {
+                "label": "B",
+                "value": 0
+            },
+            {
+                "label": "C",
+                "value": 32.807804682612
+            },
+            {
+                "label": "D",
+                "value": 196.45946739256
+            },
+            {
+                "label": "E",
+                "value": 0.19434030906893
+            },
+            {
+                "label": "F",
+                "value": -98.079782601442
+            },
+            {
+                "label": "G",
+                "value": -13.925743130903
+            },
+            {
+                "label": "H",
+                "value": -5.1387322875705
+            }
+        ]
+    }]
 
 }]);
 
